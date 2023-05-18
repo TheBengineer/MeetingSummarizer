cece5893538f03867611e4771532b161e487d6b1
Ben Holleran <wilddoogyplumb@gmail.com>
2023-05-18 14:39:02 -0400

    updated encoding

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def load_calendar_csv(filename):
     project_dir = os.getcwd()
     filepath = os.path.join(project_dir, filename)
     if os.path.isfile(filepath):
-        with open(filepath, 'r') as csvfile:
+        with open(filepath, 'r', encoding="windows-1252", errors="ignore") as csvfile:
             reader = csv.DictReader(csvfile)
             return list(reader)
 
