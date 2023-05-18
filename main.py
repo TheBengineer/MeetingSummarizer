e9a93510f350280e540e9c078430cb60537fb1be
Ben Holleran <wilddoogyplumb@gmail.com>
2023-05-18 13:38:11 -0400

    Added a test.

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+import datetime as dt
+import win32com.client
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_appointments(calendar, subject_kw=None, exclude_subject_kw=None, body_kw=None):
+    if subject_kw == None:
+        appointments = [app for app in calendar]
+    else:
+        appointments = [app for app in calendar if subject_kw in app.subject]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if exclude_subject_kw != None:
+        appointments = [app for app in appointments if exclude_subject_kw not in app.subject]
+    cal_subject = [app.subject for app in appointments]
+    cal_start = [app.start for app in appointments]
+    cal_end = [app.end for app in appointments]
+    cal_body = [app.body for app in appointments]
+
+    df = pd.DataFrame({'subject': cal_subject,
+                       'start': cal_start,
+                       'end': cal_end,
+                       'body': cal_body})
+    return df
+
+
+def get_calendar(begin, end):
+    outlook = win32com.client.Dispatch('Outlook.Application').GetNamespace('MAPI')
+    calendar = outlook.getDefaultFolder(9).Items
+    calendar.IncludeRecurrences = True
+    calendar.Sort('[Start]')
+
+    restriction = "[Start] >= '" + begin.strftime('%m/%d/%Y') + "' AND [END] <= '" + end.strftime('%m/%d/%Y') + "'"
+    calendar = calendar.Restrict(restriction)
+    return calendar
+
+
+cal = get_calendar(dt.datetime(2020, 1, 1), dt.datetime(2020, 7, 31))
